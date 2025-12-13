11210f86ac82622289a96a8c463f9d0375d978a4
damvih <damvih@gmail.com>
2025-12-14 01:17:55 +1000

    feat: use Security context repository to manage sessions

--- src/main/java/com/damvih/authentication/service/AuthenticationStateHandlerService.java
+++ src/main/java/com/damvih/authentication/service/AuthenticationStateHandlerService.java
@@ -3,18 +3,19 @@ package com.damvih.authentication.service;
 import com.damvih.authentication.dto.UserDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.util.Supplier;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationStateHandlerService {
+
+    private final SecurityContextRepository securityContextRepository;
 
     public UserDto onAuthenticated(Supplier<Authentication> supplier, HttpServletRequest request, HttpServletResponse response) {
         Authentication authentication = supplier.get();
@@ -23,11 +24,11 @@ public class AuthenticationStateHandlerService {
     }
 
     protected void perform(Authentication authentication, HttpServletRequest request, HttpServletResponse response) {
-        SecurityContext securityContext = SecurityContextHolder.getContext();
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
         securityContext.setAuthentication(authentication);
 
-        HttpSession session = request.getSession(true);
-        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, securityContext);
+        SecurityContextHolder.setContext(securityContext);
+        securityContextRepository.saveContext(securityContext, request, response);
     }
 
 }
--- src/main/java/com/damvih/common/security/config/SecurityConfig.java
+++ src/main/java/com/damvih/common/security/config/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -37,6 +39,11 @@ public class SecurityConfig {
                 )
                 .formLogin(AbstractHttpConfigurer::disable)
                 .build();
+    }
+
+    @Bean
+    public SecurityContextRepository securityContextRepository() {
+        return new HttpSessionSecurityContextRepository();
     }
 
     @Bean
