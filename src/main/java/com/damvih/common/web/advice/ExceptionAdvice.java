68c85f5c2a74be19113fa1a13501a16dfdf9b504
damvih <damvih@gmail.com>
2025-12-17 20:24:52 +1000

    fix: remove using ResponseStatus annotation

--- src/main/java/com/damvih/common/web/advice/ExceptionAdvice.java
+++ src/main/java/com/damvih/common/web/advice/ExceptionAdvice.java
@@ -5,10 +5,10 @@ import com.damvih.storage.exception.*;
 import com.damvih.authentication.exception.UserAlreadyExistsException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -16,78 +16,86 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionAdvice {
 
     @ExceptionHandler(UserAlreadyExistsException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponseDto handleDataIntegrityViolationException(UserAlreadyExistsException exception) {
+    public ResponseEntity<ErrorResponseDto> handleDataIntegrityViolationException(UserAlreadyExistsException exception) {
         log.info(exception.getMessage());
-        return new ErrorResponseDto("User already exists.");
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErrorResponseDto("User already exists."));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponseDto handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         String message = exception.getBindingResult().getFieldErrors()
                 .stream()
                 .map(FieldError::getDefaultMessage)
                 .findFirst()
                 .orElse("Validation failed.");
 
-        return new ErrorResponseDto(message);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponseDto(message));
     }
 
     @ExceptionHandler(InvalidPathException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponseDto handleInvalidPathException(InvalidPathException exception) {
+    public ResponseEntity<ErrorResponseDto> handleInvalidPathException(InvalidPathException exception) {
         log.info(exception.getMessage());
-        return new ErrorResponseDto("Invalid path.");
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponseDto("Invalid path."));
     }
 
     @ExceptionHandler(InvalidQueryException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponseDto handleInvalidQueryException(InvalidQueryException exception) {
+    public ResponseEntity<ErrorResponseDto> handleInvalidQueryException(InvalidQueryException exception) {
         log.info(exception.getMessage());
-        return new ErrorResponseDto("Invalid query for searching.");
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponseDto("Invalid query for searching."));
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponseDto handleResourceNotFoundException(ResourceNotFoundException exception) {
+    public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException exception) {
         log.info(exception.getMessage());
-        return new ErrorResponseDto("Resource not found.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponseDto("Resource not found."));
     }
 
     @ExceptionHandler(ParentDirectoryNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponseDto handleParentDirectoryNotFoundException(ParentDirectoryNotFoundException exception) {
+    public ResponseEntity<ErrorResponseDto> handleParentDirectoryNotFoundException(ParentDirectoryNotFoundException exception) {
         log.info(exception.getMessage());
-        return new ErrorResponseDto("Parent directory not found.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponseDto("Parent directory not found."));
     }
 
     @ExceptionHandler(DirectoryAlreadyExistsException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponseDto handleDirectoryAlreadyExistsException(DirectoryAlreadyExistsException exception) {
+    public ResponseEntity<ErrorResponseDto> handleDirectoryAlreadyExistsException(DirectoryAlreadyExistsException exception) {
         log.info(exception.getMessage());
-        return new ErrorResponseDto("Directory already exists.");
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErrorResponseDto("Directory already exists."));
     }
 
     @ExceptionHandler(ResourceTypesNotMatchesException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponseDto handleResourceTypesNotMatchesException(ResourceTypesNotMatchesException exception) {
+    public ResponseEntity<ErrorResponseDto> handleResourceTypesNotMatchesException(ResourceTypesNotMatchesException exception) {
         log.info(exception.getMessage());
-        return new ErrorResponseDto("Resource types are not match.");
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponseDto("Resource types are not match."));
     }
 
     @ExceptionHandler(TargetResourceAlreadyExistsException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponseDto handleTargetResourceAlreadyExistsException(TargetResourceAlreadyExistsException exception) {
+    public ResponseEntity<ErrorResponseDto> handleTargetResourceAlreadyExistsException(TargetResourceAlreadyExistsException exception) {
         log.info(exception.getMessage());
-        return new ErrorResponseDto("Target resource already exists.");
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErrorResponseDto("Target resource already exists."));
     }
 
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponseDto handleException(Exception exception) {
+    public ResponseEntity<ErrorResponseDto> handleException(Exception exception) {
         log.info("Unknown exception: {}.", exception.getMessage());
-        return new ErrorResponseDto("Internal server error.");
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponseDto("Internal server error."));
     }
 
 }
