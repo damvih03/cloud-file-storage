ea6b56ee6ff35e4a5c654ea847d90e1043242531
damvih <damvih@gmail.com>
2025-12-18 15:15:51 +1000

    fix: handle BadCredentials exception

--- src/main/java/com/damvih/common/web/advice/ExceptionAdvice.java
+++ src/main/java/com/damvih/common/web/advice/ExceptionAdvice.java
@@ -6,6 +6,7 @@ import com.damvih.authentication.exception.UserAlreadyExistsException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +15,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j
 public class ExceptionAdvice {
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponseDto> handleBadCredentialsException(BadCredentialsException exception) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponseDto("Invalid username or password."));
+    }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleDataIntegrityViolationException(UserAlreadyExistsException exception) {
