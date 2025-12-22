aada1ae410f03b018ea19cbd8f6de52426c81a2a
damvih <damvih@gmail.com>
2025-12-23 01:10:16 +1000

    feat: log exception in BucketInitializer

--- src/main/java/com/damvih/storage/util/BucketInitializer.java
+++ src/main/java/com/damvih/storage/util/BucketInitializer.java
@@ -7,8 +7,10 @@ import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class BucketInitializer {
@@ -32,6 +34,7 @@ public class BucketInitializer {
                 );
             }
         } catch (Exception exception) {
+            log.error(exception.getMessage(), exception);
             throw new StorageOperationException("Failed to create bucket with name: " + minioClientProperties.getBucketName() + ".");
         }
     }
